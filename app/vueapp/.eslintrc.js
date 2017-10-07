939f63bffdcc88e29f19e5dd8df82978b1593b1d
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-10-06 23:16:08 -0700

    Changing eslint settings. I didn't like the stock linter settings.

--- app/vueapp/.eslintrc.js
+++ app/vueapp/.eslintrc.js
@@ -4,19 +4,37 @@ module.exports = {
   parserOptions: {
     sourceType: 'module'
   },
+  env: {
+    browser: true
+  },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'standard'
+  ],
   // required to lint *.vue files
   plugins: [
-    'html'
+    'html',
+    'import'
   ],
+  globals: {
+    'cordova': true,
+    'DEV': true,
+    'PROD': true,
+    '__THEME': true
+  },
   // add your custom rules here
   'rules': {
+    "indent": 4,
     // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
+    'one-var': 0,
+    'import/first': 0,
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'brace-style': [2, 'stroustrup', { 'allowSingleLine': true }]
   }
 }
