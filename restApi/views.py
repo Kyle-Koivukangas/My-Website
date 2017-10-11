a600422f92784c420a968657053e34d91c4b5725
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-10-10 21:01:27 -0700

    refactored to use generic base classes. Django Rest Framwork's generic base classes like ListCreateAPIView make the rest API much quicker and easier to write, as you can see it uses hardly any code.

--- restApi/views.py
+++ restApi/views.py
@@ -2,41 +2,20 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, mixins, generics
+from rest_framework import status, generics
 
 from .models import User
 from .serializers import UserSerializer
 
 # Lists all users or creates a new one
-# the base class is now based on GenericAPIView and the ListModelMixin and CreateModelMixin are mixins that provide the list() and create() actions respectively.
-class UserList( mixins.ListModelMixin,
-                mixins.CreateModelMixin,
-                generics.GenericAPIView):
+# uses DRF ListCreateAPIView base class for get and create functionality right out of the box
+class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 # show info for just one user
-# base class is GenericAPIView and mixins are Retrieve, Update and Destroy actions
-class UserDetail(   mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+# Uses DRF RetrieveUpdateDestroyAPIView base class for retrieve, update, and destroy functionality right out of the box
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
 
