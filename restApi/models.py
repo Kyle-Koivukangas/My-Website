58c31a22d1af03ebae7c05c185bbde57b321ff6d
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-10-10 18:57:58 -0700

    added User model to restApi app

--- restApi/models.py
+++ restApi/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=24)
+    password = models.CharField(max_length=50)
+    email = models.EmailField()
+    isVerified = models.BooleanField()
+    superuser = models.BooleanField()
+
+
+    def __str__(self):
+        return self.username
