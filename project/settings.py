43dcd25c18d3201956c2fc37f4c6120c78b087d5
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-10-10 18:34:46 -0700

    Can't have an underscore in app name, won't work with DRF. Django Rest Framework was not liking having an underscore in the app name and I was unable to install the app properly with app.appConfig path, so I had to remake the rest_api app and remove the underscore.

--- project/settings.py
+++ project/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_api.apps.Rest_apiConfig',
+    'restApi.apps.RestapiConfig',
 ]
 
 MIDDLEWARE = [
