500d5da60559b546e982ea58ba2cd0c6e5d2343c
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-10-10 20:35:51 -0700

    Added userDetail view for seeing specific user info via api You can specify which users info to see by inputting the index after user, so user/2 to see the user at index 2.

--- project/urls.py
+++ project/urls.py
@@ -25,7 +25,8 @@ from restApi import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('app.urls')),
-    url(r'^users/', views.UserList.as_view()),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
 ] 
 
 urlpatterns = format_suffix_patterns(urlpatterns)
