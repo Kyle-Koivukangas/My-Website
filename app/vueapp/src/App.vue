8c66725cf034da0c70e319bf2b8a7ecc96e18af3
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-11-14 16:13:52 -0800

    Revert to original Nav bar... I decided to revert to original nav bar and to add in my own custom animations to avoid the whole transform on fixed elements problem. The whole thing looks MUCH cleaner now with smooth animations.
    Side bar slides in from left and the contents section (vue router component) shrinks to make room for the sidebar and vice versa when you click off to close the sidebar.

--- app/vueapp/src/App.vue
+++ app/vueapp/src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="app">
+    <!-- debug stuff (REMOVE BEFORE DEPLOY) -->
+    <!-- <p>SHOW VALUE: {{ showNav }}</p> -->
+
     <!-- <img src="./assets/logo.png"> -->
-    <!-- <vue-nav></vue-nav> -->
-    <vue-navigation></vue-navigation>
-    <router-view/>
+    <vue-nav @show="showNav = $event"></vue-nav>
+    <!-- <vue-navigation></vue-navigation> -->
+    <router-view class="content" :class="{pushRight: showNav, pushLeft: !showNav, widthLess: showNav}"/>
   </div>
 </template>
 
@@ -13,7 +16,24 @@ export default {
     components: {
         vueNav: () => import("./components/Nav.vue"),
         vueNavigation: () => import("./components/Navigation.vue"),
-    }
+    },
+    data() {
+        return {
+            showNav: false,
+        }
+    },
+    methods: {
+        navTransform(show) {
+            if (show === true) {
+
+            } if (show === false) {
+
+            } else {
+                console.log("invalid 'show' property; show must be boolean value");
+            }
+        }
+    },
+
 };
 </script>
 
@@ -25,5 +45,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 0px;
+}
+.pushRight {
+  transition: all ease 1s, width ease 1s;
+  -webkit-transition: all ease 1s, width ease 1;
+  transform: translateX(300px);
+  //   display: block;
+  width: calc(100% - 300px);
+}
+.pushLeft {
+  transition: all ease 1s, width ease 1s;
+  -webkit-transition: all ease 1s, width ease 1;
+  transform: translateX(0px);
+  width: 100%;
 }
 </style>
--- app/vueapp/src/components/Nav.vue
+++ app/vueapp/src/components/Nav.vue
@@ -3,12 +3,30 @@
 
         <transition name="slideLeft">
             <div class="sidebar fixed-top" v-if="show">
-                <h1>SIDEBAR</h1>
-                <ul>
-                    <li>LINK1</li>
-                    <li>LINK2</li>
-                    <li>LINK3</li>
-                </ul>
+                <!-- <div class="portrait-wrapper"> -->
+                <img class="portrait" src="../assets/Kyle.jpg" alt="">
+                <!-- </div> -->
+                <h3>Kyle Koivukangas</h3>
+
+                <div class="nav-links">
+                    <ul>
+                        <li>About</li>
+                        <li>Projects</li>
+                        <li>Blog</li>
+                        <li>Hire Me</li>
+                    </ul>
+                </div>
+
+                <div class="secondary-info">
+                    <ul>
+                        <li>Email</li>
+                        <li>Github</li>
+                        <li>StackOverflow</li>
+                        <li>Quora</li>
+                        <li>PGP</li>
+                        <li>BTC</li>
+                    </ul>
+                </div>
                 <button @click="show = !show">close</button>
             </div>
         </transition>
@@ -19,8 +37,9 @@
             </div>
         </transition>
 
-        <button @click="show = !show">test</button>
-        <p>show = {{ show }}</p>
+        <!-- debug stuff (REMOVE BEFORE DEPLOY) -->
+        <!-- <button @click="show = !show">test</button>
+        <p>show = {{ show }}</p> -->
 
 
     </div>
@@ -41,8 +60,12 @@ export default {
             //this watcher is just meant to delay how long it takes for the navigation 'hamburger' button to reappear after closing the sidebar.
             if (show === true) {
                 this.showBtn = false;
+                this.$emit('show', true);
             } else {
-                setTimeout(() => this.showBtn = true, 650)
+                this.$emit('show', false)
+                setTimeout(() => {
+                    this.showBtn = true;
+                }, 650);
             }
         },
     },
@@ -74,7 +97,9 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+$sidebarWidth: 300px;
+
 .fixed-top {
   position: fixed;
   top: 0;
@@ -86,11 +111,42 @@ export default {
 .nav-btn {
   left: 0;
   width: 50px;
+  z-index: 10;
 }
 .sidebar {
-  background-color: #555;
+  background-color: $white;
   left: 0;
-  width: 300px;
+  width: $sidebarWidth;
   height: 100%;
+  z-index: 11;
+  & ul {
+    list-style: none;
+    padding: 0;
+  }
+}
+.portrait {
+  border-radius: 50%;
+  max-width: 50%;
+  max-height: 50%;
+  margin-top: $sidebarWidth/4;
+}
+.nav-links {
+  font-family: $lato;
+  width: 100%;
+  height: 100%;
+  margin-top: 50px;
+  line-height: 2.6em;
+  font-size: 1.1em;
+  font-weight: 100;
+}
+.secondary-info {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: $sidebarWidth;
+  text-align: right;
+  font-family: $ubuntucond;
+  font-weight: 600;
+  color: $lightgrey;
 }
 </style>
