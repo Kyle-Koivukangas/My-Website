7abcd0c26f5cce1e8bdcf4fe49e7c656366622c0
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-11-28 20:22:54 -0800

    installed Axios and Vuex Axios for API calls and vuex to manage/store authentication state. Also set up Axios baseURL configuration.

--- app/vueapp/src/main.js
+++ app/vueapp/src/main.js
@@ -5,10 +5,31 @@ import Icon from 'vue-awesome/components/Icon';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+import Vuex from 'vuex';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 require('vue2-animate/dist/vue2-animate.min.css')
 
 Vue.component('icon', Icon);
+
+Vue.use(Vuex)
+
+Vue.use(VueAxios, axios)
+//defining Axios instance for API calls to the back end
+//and checking to see what environment it's run in (dev environment would be localhost:8000)
+if (window.location.origin == "http://localhost:8000") {
+    var API = axios.create({
+        baseURL: 'http://127.0.0.1:8000/api/'
+    })
+} else {
+    var API = axios.create({
+        baseURL: 'http://kylekoivukangas.com/api/'
+    })
+}
+
+// axios.defaults.baseURL = 'https://api.example.com';
+
 
 Vue.config.productionTip = false;
 
