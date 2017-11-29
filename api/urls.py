ad6a705017b728656ef725d06cdee682a51a5cd4
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-11-28 19:18:07 -0800

    Added permission decorators to API views

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,8 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r'projects', views.ProjectViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'users', views.UserViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- api/views.py
+++ api/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
 from api.serializers import UserSerializer, GroupSerializer, ProjectSerializer
 from api.models import Project
 
-
+@permission_classes((IsAdminUser, ))
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -13,6 +15,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
+@permission_classes((IsAdminUser, ))
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
@@ -20,6 +23,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+@permission_classes((IsAuthenticatedOrReadOnly, ))
 class ProjectViewSet(viewsets.ModelViewSet):
     """
     Projects API endpoint.
