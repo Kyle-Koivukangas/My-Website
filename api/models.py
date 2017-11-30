d9e7ba2c2d1608c92c515138bb4a7cea269184e1
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2017-11-29 21:51:08 -0800

    DRF authentication tokens are all set up. Auth tokens are working as intended, just need to set up vue login component and make it save the token to the store.

--- api/models.py
+++ api/models.py
@@ -6,7 +6,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
-
+#TODO: maybe move auth token receiver to an 'accounts' app in future if account related things get more complicated.
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from rest_framework.authtoken import views as auth_views
+from rest_framework.authtoken import views as rest_framework_views
 
 from api import views
 
@@ -8,10 +8,11 @@ router = routers.DefaultRouter()
 router.register(r'projects', views.ProjectViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'users', views.UserViewSet)
-router.register(r'token-auth', auth_views.obtain_auth_token, 'authenticate')
+# router.register(r'token-auth', rest_framework_views.obtain_auth_token, base_name='get_auth_token')
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    url(r'^api/', include(router.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^get_auth_token/$', rest_framework_views.obtain_auth_token, name='get_auth_token')
 ]
