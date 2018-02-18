54cab674a3d2b076004a9052759b1465fd652ffc
Kyle-Koivukangas <kyle.koivukangas@gmail.com>
2018-02-18 00:36:37 -0800

    Adding staging specific settings import to settings module __init__.py

--- myWebsite/settings/__init__.py
+++ myWebsite/settings/__init__.py
@@ -2,6 +2,13 @@
 try: 
     from .local import *
 except:
-    from .base import *
+    # from .base import *
 
     from .production import *
+
+# Import staging specific settings if they exist 
+# (staging.py is manually placed in the settings module on the staging server)
+try:
+    from .staging import *
+except:
+    pass
